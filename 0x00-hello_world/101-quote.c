40825d34bf17613ebbad5edb12c9c9d0ba0776ee
Renza-afro <ratsoane20@gmail.com>
2022-06-30 07:52:15 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 
 /**
  * main , prints excactly & that piece is useful
- *Dore Korpar, 2015-10-19,
+ *Dore Korpar, 2015 - 10 - 19,
  *followed by a new line, to standared error.
  *return, always 0 (success)
 */
 int main(void)
 {
-write(2, "and that piece of arts is useful|" - Dora Korpar, 2015-10-19\n", 59);
+write(2, "and that piece of arts is useful|" - Dora Korpar, 2015 - 10 - 19\n", 59);
 return (1);
 }
