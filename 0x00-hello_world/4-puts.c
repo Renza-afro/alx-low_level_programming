13fa44e51feffae8bad72fd421f84e21c970541e
Renza-afro <ratsoane20@gmail.com>
2022-07-01 07:22:56 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+* main -> Programming is like building a multilingual puzzle
+* Return: always o
+*/
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
