e6615dfa2b0b82b403de29aafaab90b74add43c7
Renza-afro <ratsoane20@gmail.com>
2022-07-02 04:30:18 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main-> prints all single of base 10 from 0, new line
+* not allowed to use char datatype
+* use putchar
+* code in the main
+* Return: always 0
+*/
+
+int main(void)
+{
+
+int num;
+for (num = 0; num < 10; num++)
+{
+putchar(num + '0');
+}
+putchar('\n')
+
+return (0);
+}
