35551212d5450c47464e41ee9e193b9977900150
Renza-afro <ratsoane20@gmail.com>
2022-07-02 04:19:28 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main-> prints all single of base 10 numbers from 0, followed by a new line
+* all code in the main
+* Return: always 0
+*/
+
+int main(void)
+{
+
+int baseTen;
+for (baseTen = 0; baseTen < 10; baseTen++)
+{
+printf("%d", baseTen);
+}
+putchar('\n');
+
+return (0);
+}
