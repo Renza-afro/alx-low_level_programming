45edfecd5b635c2a6fa4f52bb74ac05d7090b25d
Renza-afro <ratsoane20@gmail.com>
2022-07-02 06:21:42 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+* main->  prints all possible combinations of two two-digit numbers
+* range fro 0 to 99
+* use putchar eight times
+* no char datatype
+* Return: always 0
+*/
+
+int main(void)
+{
+int x, y, z, v;
+for (x = 48; x = 57; x++)
+{
+for (y = 48; y <= 57; y++)
+{
+for (z = 48; z <= 57; z++)
+{
+for (v = 48; v <= 57; v++)
+{
+if (((z + v) > (x + y) && z >= x) || x < z)
+{
+putchar(x);
+putchar(y);
+putchar(' ');
+putchat(z);
+putchar(v);
+if (x + y + z + v == 227 && c == 57)
+{
+break;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n);
+
+return (0);
+}
+
