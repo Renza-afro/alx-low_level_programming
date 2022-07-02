638b1ac75af9a0db9d73f21c36948fa3e4dda8c8
Renza-afro <ratsoane20@gmail.com>
2022-07-02 04:42:35 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main -> prints the lowercase alphabet in reverse, a new line
+* use putchar
+* use putchar twice
+* Return: always 0
+*/
+
+int main(void)
+{
+
+int alphabetReverse;
+for (alphabetReverse = 'z'; alphabetReverse >= 'a'; alphabetReverse--)
+{
+putchar(a);
+}
+putchar('\n');
+
+return (0);
+}
