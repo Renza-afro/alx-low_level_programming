5731ea80c15be7e7796a364a41fd2c962971c37d
Renza-afro <ratsoane20@gmail.com>
2022-07-05 05:44:24 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main,h"
+
+/**
+* _isalpha-> checks for alphabetic character
+* Prototype: int _isalpha(int c)
+* @c: The character in ASCII code
+* Return: 1 for letters. 0 for the rest
+*/
+
+int _isalpha(int c)
+{
+
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
