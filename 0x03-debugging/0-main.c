60bd6d7f40fd74de2bda0da56b99bf9206436044
Renza-afro <ratsoane20@gmail.com>
2022-07-06 04:44:34 -0700

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,26 @@
 #include "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void postitive_or_negative(int i)
+{
+
+int n;
+srand(time(0));
+n = i;
+if (n > 0)
+printf("%d is positive\n", n);
+
+if (n == 0)
+printf("%d is zero\n", n);
+
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+return (0);
+}
 
 /**
 * main-> file is a test for a postitive or negative
@@ -8,7 +30,8 @@
 int main(void)
 {
 
-int i = 0;
+int i;
+i = 0;
 postitive_or_negative(i);
 
 return (0);
