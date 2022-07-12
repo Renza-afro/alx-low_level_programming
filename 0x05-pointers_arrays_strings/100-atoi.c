f1d6f88c0eb72ec451b3b335993ce8594f6306fb
Renza-afro <ratsoane20@gmail.com>
2022-07-12 07:04:48 -0700

    task 10. Great leaders are willing to sacrifice the numbers to save the people

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -4,30 +4,28 @@
 /**
 * _atoi-> convert a string to an integer
 * @s: pointer
-* Return: res
+* Return: int
 */
 
 int _atoi(char *s)
 {
 
-int i;
-int res = 0;
-int sig = -1;
-int brk = 0;
 
-for (i = 0; s[i] != '\0'; i++)
+int i, j, z, x;
+
+i = z = 0;
+x = 1;
+while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
 {
 if (s[i] == '-')
-sig = sig * -1;
-if (s[i] >= '0' && s[i] <= '9')
+x *= -1;
+i++;
+}
+j = i;
+while ((s[j] >= '0') && (s[j] <= '9'))
 {
-res = res * 10;
-res -= (s[i] - '0');
-brk = 1;
+z = (z * 10) + x * ((s[j]) - '0');
+j++;
 }
-else if (brk == 1)
-break;
-}
-res = sig * res;
-return (res);
+return (z);
 }
