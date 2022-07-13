1d65f58e635dfba628629cd99f01d00211c2d096
Renza-afro <ratsoane20@gmail.com>
2022-07-13 03:14:23 -0700

    task 1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* *_strncat-> concatenates two strings
+* @dest: param 1
+* @src: param 2
+* @n: integer
+* Return: dest
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+strcat(dest, src, n);
+return (dest);
+}
