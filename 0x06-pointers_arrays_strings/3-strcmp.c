ad932e086372c13e8737925befe20593688e914f
Renza-afro <ratsoane20@gmail.com>
2022-07-13 04:33:06 -0700

    task 3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strcmp-> compares two strings
+* @s1: param 1
+* @s2: param 2
+* Return: integer
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+
+int result;
+
+result = strcmp(s1, s1);
+return (result);
+}
