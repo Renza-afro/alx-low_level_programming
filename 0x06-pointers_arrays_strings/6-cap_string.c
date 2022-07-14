69ee86ce4eb86f5bad2c8435dd9caec46f494eda
Renza-afro <ratsoane20@gmail.com>
2022-07-14 05:23:43 -0700

    task 6. Expect the best. Prepare for the worst

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
 * *cap_string-> capitalizes all words of a string
 * @i: string
@@ -9,17 +9,17 @@
 char *cap_string(char *i)
 {
 int a = 0, x;
-int cspc = 13;
+int len = 13;
 char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
 
 while (i[a])
 {
 x = 0;
 
-while (x < cspc)
+while (x < len)
 {
 if ((a == 0 || i[a - 1] == spc[i]) && (i[a] >= 97 && i[a] <= 122))
-i[a] -= 32;
+i[a] = i[a] - 32;
 
 x++;
 }
