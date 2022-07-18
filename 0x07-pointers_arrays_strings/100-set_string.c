c404bfc79cb003a0b707f671e4cb6ff28131f025
Renza-afro <ratsoane20@gmail.com>
2022-07-18 05:31:13 -0700

    task 8. double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* set_string-> ses value of a pointer to char
+* @s: string
+* @to: string to char
+* Return: nothing
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
