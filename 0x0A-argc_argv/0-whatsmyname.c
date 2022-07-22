743618bf8189866669d51b4c42b6b9f4b4921db6
Renza-afro <ratsoane20@gmail.com>
2022-07-22 08:26:29 -0700

    0. It ain't what they call you

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main-> print its name
+* @argc: param
+* @argv: agurment
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+
+return (0);
+}
