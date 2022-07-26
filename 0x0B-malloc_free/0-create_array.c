68d3db9eeac962619143ee4287ddaa8f8de16ae2
Renza-afro <ratsoane20@gmail.com>
2022-07-26 02:29:55 -0700

    task 0. float like a butterfly

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* create_array-> create an array
+* @size: array size
+* @c: array character
+* Return: null if size is 0
+*/
+
+char *create_array(unsigned int size, char c)
+{
+unsigned int x;
+char *s;
+
+if (size == 0)
+return (NULL);
+s = (cahr *)malloc(size * sizeof(char));
+if (s == NULL)
+return (NULL);
+for (x = 0; x < size; x++)
+s[x] = c;
+return (s);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+0x0B-malloc free in alx low level programming
