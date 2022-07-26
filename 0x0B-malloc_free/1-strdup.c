0bdefc2f37487b43c87439f7a71eeecdf0d77880
Renza-afro <ratsoane20@gmail.com>
2022-07-26 03:17:32 -0700

    task 1. The woman who has no imagination

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strdup-> returns a pointer to allocated space
+* @str:string
+* Return: NULL if str null
+*/
+
+char *_strdup(char *str)
+{
+int x = 1, y = 0;
+char *s;
+if (str == NULL)
+return (NULL);
+while (str[x])
+x++;
+s = (char *)malloc(x *sizeof(char) + 1);
+if (s == NULL)
+return (NULL);
+while (y < x)
+{
+s[y] = str[y];
+y++;
+}
+s[y] = '\0';
+return (s);
+}
