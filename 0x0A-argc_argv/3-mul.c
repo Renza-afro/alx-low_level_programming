cad82f2fb703ec03f0ea5117e193c26c7d2d0723
Renza-afro <ratsoane20@gmail.com>
2022-07-26 23:50:58 -0700

    task 3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -3,22 +3,18 @@
 #include <stddef.h>
 /**
 * main - Prints the multiplication of two args numbers
-* @argc: argument count
-* @argv: argument vector
+* @x: argument count
+* @y: args
 * Return: 1 if error. 0
 */
-int main(int argc, char *argv[])
+int main(int x, char **y)
 {
-int result, num1, num2;
-if (argc != 3)
+if (x < 3)
 {
-printf("%s\n", "Error");
+printf("Error'\n'");
 return (1);
 }
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
-result = num1 * num2;
 
-printf("%d\n", result);
+printf("%d\n", atoi(*(y + 1)) * atoi(*(y + 2)));
 return (0);
 }
