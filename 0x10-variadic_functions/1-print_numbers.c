a93ddfd4af750c5db47fdd379892df5a40e618f9
Renza-afro <ratsoane20@gmail.com>
2022-08-04 02:32:15 -0700

    task 1. 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_function.h"
+
+/**
+* print_numbers-> print numbers, new line
+* @separator: .....
+* @n: integers
+* Return: no
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list vari_list;
+unsigned int x;
+
+va_start(vari_list, n);
+for (x = 0; x < n; x++)
+{
+printf("%d", va_arg(vari_list, int));
+if (separator && x < n - 1)
+printf("%s", separator);
+}
+printf("\n");
+va_end(vari_list);
+}
