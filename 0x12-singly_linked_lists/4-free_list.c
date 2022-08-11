a67e330a36603d6c6362fa7bf3e9eb841ea05054
Renza-afro <ratsoane20@gmail.com>
2022-08-11 06:57:36 -0700

    task 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+* free_list-> free the list
+* @head: ..
+* Return: no
+*/
+
+void free_list(list_t *head)
+{
+list_t *temp;
+if (head)
+{
+while (head)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
+}
+}
