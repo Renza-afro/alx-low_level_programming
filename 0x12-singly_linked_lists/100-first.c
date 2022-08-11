a54a604ef0c6af6943bc756d9b77721ef6aa3e88
Renza-afro <ratsoane20@gmail.com>
2022-08-11 07:07:15 -0700

    task 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+* before_main_method-> execute before main method
+* Return: no
+*/
+void before_main_method(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
