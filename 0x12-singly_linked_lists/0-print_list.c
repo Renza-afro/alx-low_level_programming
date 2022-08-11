ee959d2dcfde1e833e500f6158a796c397626112
Renza-afro <ratsoane20@gmail.com>
2022-08-11 07:21:21 -0700

    task 0-print_list.c fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,10 +8,10 @@
 
 size_t print_list(const list_t *h)
 {
-unsigned int x = 0;
+unsigned int i = 0;
 const list_t *temp_h = h;
 
-for (; temp_h; temp_h = temp_h->next, x++)
+for (; temp_h; temp_h = temp_h->next, i++)
 printf("[%d] %s\n", temp_h->len, temp_h->str);
-return (x);
+return (i);
 }
