ef31cd24fa16c96949ec2470d5744d1616f4a06c
Renza-afro <ratsoane20@gmail.com>
2022-08-11 04:25:12 -0700

    task 0-print_lists.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+* print_list-> prints all elements of a list_t
+* @h: ..
+* Return: nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+unsigned int x = 0;
+const list_t *temp_h = h;
+for (; temp_h; temp_h = tem_h->next, x++)
+printf("[%d] %s\n", temp_h->len, temp_h->str);
+return (x);
+}
