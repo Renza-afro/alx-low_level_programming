913bf3801b4fcf961fc976d1a2d1054ff37019fa
Renza-afro <ratsoane20@gmail.com>
2022-08-15 02:49:03 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* print_listint-> prints all elements of listint_t
+* @h: head
+* Return: nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+unsigned int x = 0;
+const listint_t *temp_h = h;
+while (temp_h)
+{
+printf("%d\n", temp_h.n);
+temp_h = temp_h.next;
+x++;
+}
+return (x);
+}
