c6530dc62af9cb6e841b28e6b47466a795ab4aed
Renza-afro <ratsoane20@gmail.com>
2022-08-16 01:21:39 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,20 +8,20 @@
 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint *index_node, *temp_node = *head;
+listint_t *index_node, *temp_node = *head;
 unsigned int x = 0;
 
 if (temp_node && (index == 0))
 {
 *head = temp_node->next;
-free(temp(temp_node);
+free(temp_node);
 return (1);
 }
 while (temp_node)
 {
 if (x + 1 == index)
 {
-index_node = node = temp_node->next;
+index_node = temp_node->next;
 if (index_node)
 {
 temp_node->next = index_node->next;
