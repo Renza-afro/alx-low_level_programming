7fb38b9f9f9b980ae31c386c70b1bf0023fe8ad0
Renza-afro <ratsoane20@gmail.com>
2022-08-16 02:24:27 -0700

    102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+* free_listint_safe-> free a listint_t
+* @h:...
+* Return: size of the list
+*/
+size_t free_listint_safe(listint_t **h)
+{
+size_t x = 0, y;
+listsint_t *temp, *chck, *copy = *h;
+
+while (*h)
+{
+x++;
+temp = *h;
+*h = (*h)->next;
+free(temp);
+chck = copy;
+y = 0;
+while (y < x)
+{
+if (*h == chck)
+{
+*h = NULL;
+return (x);
+}
+chck = chck->next;
+y++;
+}
+}
+return (x);
+}
