1f24eb1087f8c519acbf16324740967a62cb4fb2
Renza-afro <ratsoane20@gmail.com>
2022-08-18 06:33:59 -0700

    fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,12 +7,7 @@
 
 void print_binary(unsigned long int n)
 {
-unsigned long int binary = 0;
-int bnr = 0;
-
-while ((binary >>= 1) > 0)
-bnr++;
-while (bnr >= 0)
-_putchar((n >> bnr--) & 1 ? '1' : '0');
-
+if (n > 1)
+print_binary(n >> 1);
+_putchar((n & 1) + '0');
 }
